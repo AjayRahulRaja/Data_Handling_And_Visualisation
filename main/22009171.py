b9f624f97aeca66e6accf05bf2e7c789bea98206
AjayRahulRaja <125606523+AjayRahulRaja@users.noreply.github.com>
2023-04-29 14:19:12 +0100

    Update 22009171.py

--- main/22009171.py
+++ main/22009171.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 14:14:08 2023
+
+@author: Ajay
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -41,7 +48,6 @@ male = male[['CODGEO', 'LIBGEO_left', 'SNHME14', 'SNHMO14', 'SNHM1814', 'SNHM261
 male = male.rename(columns={'CODGEO':'CODGEO', 'LIBGEO_left':'LIBGEO', 'SNHME14':'net_salary_per_hour_employee', 'SNHMO14':'net_salary_per_hour_worker', 'SNHM1814':'net_salary_18_to_25', 'SNHM2614':'net_salary_26_to_50' ,'SNHMH5014':'net_salary_more_than_50yrs'})
 male = male.drop_duplicates()
 male_top_10 = male.sort_values(['net_salary_per_hour_employee', 'net_salary_per_hour_worker'], ascending=False).head(10)
-male.head()
 
 # filtering female data
 female = salary_per_town_categories_merge[salary_per_town_categories_merge['SEXE'] == 2]
@@ -49,9 +55,49 @@ female = female[['CODGEO', 'LIBGEO_left', 'SNHMFE14', 'SNHMFO14', 'SNHMF1814', '
 female = female.rename(columns={'CODGEO':'CODGEO', 'LIBGEO_left':'LIBGEO', 'SNHMFE14':'net_salary_per_hour_employee', 'SNHMFO14':'net_salary_per_hour_worker', 'SNHMF1814':'net_salary_18_to_25', 'SNHMH2614':'net_salary_26_to_50' ,'SNHMF5014':'net_salary_more_than_50yrs'})
 female = female.drop_duplicates()
 female_top_10 = female.sort_values(['net_salary_per_hour_employee', 'net_salary_per_hour_worker'], ascending=False).head(10)
-female.head()
 
+expensive_cities = salary_per_town_categories[salary_per_town_categories['SNHM14'] >= 22]
+expensive_cities_1 = expensive_cities[['LIBGEO', 'SNHM14']]
+expensive_cities_2 = expensive_cities_1.sort_values('SNHM14', ascending=False).head(10)
 
+pas_cher_cities = salary_per_town_categories[salary_per_town_categories['SNHM14'] <= 22]
+pas_cher_cities_1 = pas_cher_cities[['LIBGEO', 'SNHM14']]
+pas_cher_cities_2 = pas_cher_cities_1.sort_values('SNHM14', ascending=False)
+pas_cher_cities_3 = pas_cher_cities_2.drop_duplicates('SNHM14').head(10)
+
+distance = name_geographic_information[['code_insee','éloignement']]
+dtype = {'CODGEO':int}
+expensive_cities=expensive_cities.astype(dtype)
+
+distance_pd_merged = pd.merge(expensive_cities, distance, how='left', left_on='CODGEO', right_on='code_insee')
+
+expensive_cities = salary_per_town_categories[salary_per_town_categories['SNHM14'] >= 22]
+expensive_cities_1 = expensive_cities[['LIBGEO', 'SNHM14']]
+expensive_cities_2 = expensive_cities_1.sort_values('SNHM14', ascending=False).head(10)
+
+pas_cher_cities = salary_per_town_categories[salary_per_town_categories['SNHM14'] <= 22]
+pas_cher_cities_1 = pas_cher_cities[['CODGEO','LIBGEO', 'SNHM14']]
+pas_cher_cities_2 = pas_cher_cities_1.sort_values('SNHM14', ascending=False)
+pas_cher_cities_3 = pas_cher_cities_2.drop_duplicates('SNHM14').head(10)
+
+distance = name_geographic_information[['code_insee','éloignement']]
+dtype = {'CODGEO':int}
+expensive_cities=expensive_cities.astype(dtype)
+
+# distance_pd_merged = pd.merge(expensive_cities, distance, how='left', left_on='CODGEO', right_on='code_insee')
+expensive_distance_pd_merged = pd.merge(expensive_cities, distance, how='left', left_on='CODGEO', right_on='code_insee')
+expensive_distance_pd_merged[['LIBGEO', 'SNHM14', 'éloignement']].sort_values('SNHM14', ascending=False)
+expensive_distance_pd_merged = expensive_distance_pd_merged[expensive_distance_pd_merged['LIBGEO'].isin(expensive_cities_2['LIBGEO'].values)]
+expensive_distance_pd_merged[['CODGEO','LIBGEO', 'SNHM14', 'éloignement']]
+
+dtype = {'CODGEO':int}
+pas_cher_cities_3=pas_cher_cities_3.astype(dtype)
+
+pas_cher_distance_pd_merged = pd.merge(pas_cher_cities_3, distance, how='left', left_on='CODGEO', right_on='code_insee')
+pas_cher_distance_pd_merged[['LIBGEO', 'SNHM14', 'éloignement']].sort_values('SNHM14', ascending=False)
+
+pas_cher_distance_pd_merged = pd.merge(pas_cher_cities_3, distance, how='left', left_on='CODGEO', right_on='code_insee')
+pas_cher_distance_pd_merged[['LIBGEO', 'SNHM14', 'éloignement']].sort_values('SNHM14', ascending=False)
 
 # plotting the graphs
 import seaborn as sns
@@ -60,103 +106,22 @@ from matplotlib.gridspec import GridSpec
 
 plt.style.use('ggplot')
 
-fig = plt.figure(figsize=(55, 20))
-grid = GridSpec(5, 8, wspace=0.05, hspace=0.325, left=0.1,
-                bottom=0.1,
-                right=0.9,
-                top=0.9, figure=fig)
+fig = plt.figure(figsize=(27, 18))
 
-ax1 = fig.add_subplot(grid[0,0])
-sns.kdeplot(male['net_salary_per_hour_worker'], shade=True, ax=ax1, label='Male')
-sns.kdeplot(female['net_salary_per_hour_worker'], shade=True, ax=ax1, label='Female')
-ax1.set(title='Gender wise density comparison of salary per hour (worker)')
-ax1.title.set_size(13)
-ax1.set_xlabel('Salary', fontsize=12)
-ax1.set_ylabel('Density', fontsize=12)
-plt.legend(fontsize=10)
+grid = GridSpec(4, 4, wspace=0.325, hspace=0.30, figure=fig)
 
-ax2 = fig.add_subplot(grid[0,1])
-plt.text(0.0, 0.6, f'\nThis graph shows the net salary of worker\nbetween different sexes and of all catagories of ages.\n\nThis graph shows the overall salary given to Men \nis greater than Women in France.')
-ax2.axis('off')
+ax1 = fig.add_subplot(grid[0, 0:3])
+plt.text(0.98, 0.95, "Name: Ajay Rahul Raja", fontsize=15, fontweight="bold")
+plt.text(0.98, 0.85, "Student ID: 22009171", fontsize=15, fontweight="bold")
+plt.text(0.15, 0.55, "Data Handling and Visualisation - Infographics Project", fontsize=30, fontweight="bold")
+plt.text(0.25, 0.25, "Which city is the best to choose in France?", fontsize=25, fontweight="bold")
+plt.axis("off")
 
-ax3 = fig.add_subplot(grid[0,2])
-sns.kdeplot(male['net_salary_per_hour_employee'], shade=True, ax=ax3, label='Male')
-sns.kdeplot(female['net_salary_per_hour_employee'], shade=True, ax=ax3, label='Female')
-ax3.set(title='Gender wise density comparison of salary per hour (employee)')
-ax3.title.set_size(13)
-ax3.set_xlabel('Salary', fontsize=12)
-ax3.set_ylabel('Density', fontsize=12)
-plt.legend(fontsize=10)
-
-ax4 = fig.add_subplot(grid[0,3])
-plt.text(0.0, 0.4, f'\nThis graph shows the Monthly salary of employees \nbetween different sexes and of all catagories of ages.\n\nBased on this graph we can understand\nthe overall salary earned by Women employees\nis greater than Men in France.\n\nComparing with the net salary of all workers, \nWomen as employee earning more than Male employees.')
-ax4.axis('off')
-
-ax5 = fig.add_subplot(grid[1,0])
-sns.kdeplot(male['net_salary_18_to_25'], shade=True, ax=ax5, label='Men between 18 and 25')
-sns.kdeplot(male['net_salary_26_to_50'], shade=True, ax=ax5, label='Men between 26 and 50')
-sns.kdeplot(male['net_salary_more_than_50yrs'], shade=True, ax=ax5, label='Men more than 50 years')
-ax5.set(title='Monthly salary for Men between different age ranges')
-ax5.title.set_size(13)
-ax5.set_xlabel('Salary', fontsize=12)
-ax5.set_ylabel('Density', fontsize=12)
-plt.legend(fontsize=10)
-
-ax7 = fig.add_subplot(grid[1,1])
-plt.text(0.0, 0.4, f'\nThis graph shows the monthly salary of Men\nbetween different sexes and of all catagories. \n\nThis graph shows the overall salary earned by \nMen around 18 and 25 earn \nrelatively more than Men aged more 50 years.')
-ax7.axis('off')
-
-ax6 = fig.add_subplot(grid[1,2])
-sns.kdeplot(female['net_salary_18_to_25'], shade=True, ax=ax6, label='Women between 18 and 25')
-sns.kdeplot(female['net_salary_26_to_50'], shade=True, ax=ax6, label='Women between 26 and 50')
-sns.kdeplot(female['net_salary_more_than_50yrs'], shade=True, ax=ax6, label='Women more than 50 years')
-ax6.set(title='Monthly salary for Women between different age ranges')
-ax6.title.set_size(13)
-ax6.set_xlabel('Salary', fontsize=12)
-ax6.set_ylabel('Density', fontsize=12)
-plt.legend(fontsize=10)
-
-ax8 = fig.add_subplot(grid[1,3])
-plt.text(0.0, 0.4, f'\nThis graph shows the monthly salary of Women\nbetween different sexes and of all catagories. \n\nThis graph shows the Women aged 18 and 25 years \nearn lesser than wommen agaed more than 50.\n\nBut larger number of women fall under the range \nbetween 18 and 25.')
-ax8.axis('off')
-
-ax9 = fig.add_subplot(grid[0,4])
-male_top_10.plot(x='LIBGEO', kind='bar', stacked=False, ax=ax9)
-plt.xlabel("Cities", fontsize=12)
-plt.ylabel("Net Salary", fontsize=12)
-ax9.set_xticklabels(ax9.get_xticklabels())
-plt.title("Net Salary of Men over different ages in different cities")
-ax9.title.set_size(13)
-plt.legend(bbox_to_anchor=(1.015, 1.005), fontsize=10)
-
-ax11 = fig.add_subplot(grid[0,5])
-plt.text(0.0, 0.0, f'\nThis graph shows the net salary of employee\nbetween different sexes and of all catagories. \n\nThis graph shows the overall salary earned by \nWomen is greater than Men in France. \n\nComparing with the net salary of all sexes, \nWomen as employee earning more than Men.')
-ax11.axis('off')
-
-ax10 = fig.add_subplot(grid[2,4])
-female_top_10.plot(x='LIBGEO', kind='bar', stacked=False, ax=ax10)
-plt.xlabel("Cities", fontsize=12)
-plt.ylabel("Net Salary", fontsize=12)
-plt.title("Net Salary of Women over different ages in different cities")
-ax10.set_xticklabels(ax10.get_xticklabels())
-ax10.title.set_size(13)
-plt.legend(bbox_to_anchor=(1.015, 1.005), fontsize=10)
-
-ax12 = fig.add_subplot(grid[2,5])
-plt.text(0.0, 0, f'\nThis graph shows the net salary of employee\nbetween different sexes and of all catagories. \n\nThis graph shows the overall salary earned by \nWomen is greater than Men in France. \n\nComparing with the net salary of all sexes, \nWomen as employee earning more than Men.')
-ax12.axis('off')
-
-ax16 = fig.add_subplot(grid[1,4])
-plt.text(0.0, 0, ' ')
-ax16.axis('off')
-
-ax13 = fig.add_subplot(grid[2,0])
+ax2 = fig.add_subplot(grid[1:3, 0:2])
 salary_per_town_categories['wage_gap'] = salary_per_town_categories['SNHMH14'] - salary_per_town_categories['SNHMF14']
 wage_vs_gap = pd.DataFrame({'Mean Wages':salary_per_town_categories['SNHM14'],
                             'Wage Gap':salary_per_town_categories['wage_gap']})
 salary_per_town_categories['percentage_wage_gap'] = (salary_per_town_categories['wage_gap'] / salary_per_town_categories['SNHM14']) * 100
-
-
 
 salary_per_town_categories.loc[salary_per_town_categories['percentage_wage_gap'] <= 0]
 salary_data=salary_per_town_categories.sort_values(by=['percentage_wage_gap'], ascending=False).head(15)
@@ -166,23 +131,75 @@ sns.barplot(x='SNHM14', y='LIBGEO', data=salary_data,
 sns.set_color_codes("muted")
 sns.barplot(x='wage_gap', y='LIBGEO', data=salary_data,
             label="Wage Gap", color="b")
-ax13.set(xlim=(0, 50))
-ax13.set_title("15 Cities in France with the Highest Percentage Wage Gap", fontsize=13)
-ax13.set_ylabel("Cities", fontsize=12)
-ax13.set_xlabel("Percentage Wage Gap", fontsize=12)
+ax2.set(xlim=(0, 50))
+ax2.set_title("15 Cities in France with the Highest Percentage Wage Gap", fontsize=13)
+ax2.set_ylabel("Cities", fontsize=12)
+ax2.set_xlabel("Percentage Wage Gap", fontsize=12)
+plt.legend(bbox_to_anchor=(0.99, 0.091), fontsize=10)
+
+ax3 = fig.add_subplot(grid[3, 0])
+expensive_distance_pd_merged=expensive_distance_pd_merged.sort_values("SNHM14")
+# Plotting the first data series on the left axis
+plot_1 = ax3.plot(expensive_cities_2['LIBGEO'], expensive_cities_2['SNHM14'], label="Expensive cities")
+plt.xticks(rotation=90)
+ax3.set_xlabel('cities')
+ax3.set_ylabel('net mean salary', color='red')
+
+# Creating the second axis on the right side
+ax31 = ax3.twinx()
+
+# Plottting the second data series on the right axis
+plot_2 = ax31.plot(expensive_distance_pd_merged['LIBGEO'], expensive_distance_pd_merged['éloignement'], label="Distance from Paris", color='blue')
+ax31.set_ylabel('Distance', color='blue')
+plt.xticks(rotation=90)
+plt.legend()
+
+plts = plot_1 + plot_2
+labels = [x.get_label() for x in plts]
+ax3.set(title="Less expensive cities than Paris")
+ax3.title.set_size(10)
+plt.legend(plts, labels, loc=0)
+
+ax4 = fig.add_subplot(grid[3, 1])
+sns.kdeplot(male['net_salary_per_hour_worker'], shade=True, ax=ax4, label='Male')
+sns.kdeplot(female['net_salary_per_hour_worker'], shade=True, ax=ax4, label='Female')
+ax4.set(title='Gender wise density comparison of salary per hour (worker)')
+ax4.title.set_size(10)
+ax4.set_xlabel('Salary', fontsize=12)
+ax4.set_ylabel('Density', fontsize=12)
+plt.legend(fontsize=10)
+
+ax5 = fig.add_subplot(grid[3, 2])
+female_top_10.plot(x='LIBGEO', kind='bar', stacked=False, ax=ax5)
+plt.xlabel("Cities", fontsize=12)
+plt.xticks(rotation=90)
+plt.ylabel("Net Salary", fontsize=12)
+plt.title("Net Salary of Women over different ages in different cities")
+ax5.set_xticklabels(ax3.get_xticklabels())
+ax5.title.set_size(10)
 plt.legend(bbox_to_anchor=(1.015, 1.005), fontsize=10)
 
-ax14 = fig.add_subplot(grid[2,1])
-plt.text(0.0, 0, f'\nThis graph shows the wage gap in all the cities in France.\nThe cities are named from 0 to 5000.\nEach number represents a city.')
-ax14.axis('off')
+ax6 = fig.add_subplot(grid[1, 2])
+plt.text(0, 0.13, "To find out which city is most suitable to live in France\nbased on the wages and cost of living.\n\nWe can infer from the analysis that Men are still earning\nmore than Women in most of the cities.\n\nParis is the captial of France and there are cities \nexpensive than paris like Croissy-sur-Seine and\ncheapest is Lamorlaye.\n\nWe can also see how many firms are ther in Paris\nwhich are actively functioning with how many employees.", fontsize=15)
+ax6.axis('off')
 
-ax15 = fig.add_subplot(grid[2,2])
-plt.plot(wage_vs_gap, label=('Mean Wages', 'Wage Gap'))
-plt.legend()
-ax15.set_title("Wage gap in all cities in France", fontsize=13)
-ax15.set_ylabel("Hourly wage gap", fontsize=12)
-ax15.set_xlabel('Cities', fontsize=12)
+ax7 = fig.add_subplot(grid[2, 2])
+base_establissement_pre_tranche = base_establissement_pre_tranche[['LIBGEO', 'E14TS1' ,'E14TS6', 'E14TS10', 'E14TS20', 'E14TS50' ,'E14TS100', 'E14TS200', 'E14TS500']]
+paris = base_establissement_pre_tranche[base_establissement_pre_tranche['LIBGEO'] == 'Paris']
+paris = paris.rename(columns={'E14TS1':"More than 1 employees",
+                              'E14TS6':"More than 6 employees",
+                              'E14TS10':"More than 10 employees",
+                              'E14TS20':"More than 20 employees",
+                              'E14TS50':"More than 50 employees",
+                              'E14TS100':"More than 100 employees",
+                              "E14TS200":"More than 200 employees",
+                              "E14TS500":"More than 500 employees"})
+paris["employees betweeen 1 to 20"] = paris["More than 20 employees"]+paris["More than 10 employees"]+paris["More than 6 employees"]+paris["More than 1 employees"]
+paris = paris.drop(columns=["More than 20 employees", "More than 10 employees", "More than 6 employees", "More than 1 employees"])
+paris = paris.set_index("LIBGEO")
+paris_values = [180, 456, 812, 1658]
+plt.pie(paris_values, autopct="%1.1f%%", labels=None, shadow=True, startangle=90)
+ax7.set_title("Percentage of firms in Paris \nclassified on the basis of number of employees", fontsize=13)
+plt.legend(labels=["More than 500 employees", "More than 200 employees", "More than 100 employees", "employees more than 50"], bbox_to_anchor=(1.05, 0.75), fontsize=10)
 
-ax15 = fig.add_subplot(grid[2,3])
-plt.text(0.0, 0.6, f'\nThis graph shows the wage gap in all the cities in France.\nThe cities are named from 0 to 5000.\nEach number represents a city.')
-ax15.axis('off')
+plt.show()
